7eaba5e54c2f1003fc7fc785f4a290624d130643
Zahid Ahmed <zahidbiscj@gmail.com>
2021-06-07 08:09:54 +0600

    IBaseRepository with BaseRepository dependency injection added

--- EShop.Server/EShop.Service/Services/CategoryService.cs
+++ EShop.Server/EShop.Service/Services/CategoryService.cs
@@ -17,10 +17,10 @@ namespace EShop.Service.Services
 {
     public class CategoryService : ICategoryService
     {
-        private readonly ICategoryRepository _categoryRepository;
+        private readonly IBaseRepository<Category,int> _categoryRepository;
         private readonly IMapper _mapper;
 
-        public CategoryService(ICategoryRepository categoryRepository, IMapper mapper)
+        public CategoryService(IBaseRepository<Category, int> categoryRepository, IMapper mapper)
         {
             _categoryRepository = categoryRepository;
             _mapper = mapper;
